4fbb501c61c96f2c3bbdb0fe4f2718e747d2484d
Nguyen Ngoc Hiep <nnhiep582k2@gmail.com>
2023-03-20 20:50:45 +0700

    feat: change some resources

--- app/src/main/java/com/nnhiep/travelmanager/database/Database.java
+++ app/src/main/java/com/nnhiep/travelmanager/database/Database.java
@@ -6,12 +6,9 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
-
 import com.nnhiep.travelmanager.R;
 import com.nnhiep.travelmanager.models.Note;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
